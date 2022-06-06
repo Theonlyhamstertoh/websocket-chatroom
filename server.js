657cdb3f06553eb3c5b0ab30d5e345991da737c9
Weibo Zhang <theonlyhamstertoh@gmail.com>
2022-06-05 20:25:10 -0700

    change to express app to test

--- server.js
+++ server.js
@@ -2,6 +2,18 @@ const UWS = require("uWebSockets.js");
 const randomColor = require("randomcolor");
 const { v4: uuidv4 } = require("uuid");
 
+var PORT = process.env.PORT || 5000;
+var express = require("express");
+var app = express();
+
+var http = require("http");
+var server = http.Server(app);
+
+app.use(express.static("client"));
+
+server.listen(PORT, function () {
+  console.log("Chat server running");
+});
 console.log("just this okay?");
 // const decoder = new TextDecoder();
 // const port = process.env.PORT || 9001;
